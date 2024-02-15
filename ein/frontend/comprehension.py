ff40974c88f873947056ba0eabc2bbcb90c69432
jbachurski <kbachurski@gmail.com>
2024-02-15 17:33:37 +0000

    Refactor folds to use the struct facilities

--- ein/frontend/comprehension.py
+++ ein/frontend/comprehension.py
@@ -1,4 +1,3 @@
-import functools
 import inspect
 from typing import (
     Any,
@@ -14,7 +13,7 @@ from typing import (
 
 from ein import calculus
 from ein.calculus import Expr
-from ein.frontend.layout import VecLayout, build_layout, fold_layout
+from ein.frontend.layout import Layout, VecLayout, build_layout, fold_layout
 from ein.midend.size_classes import _dim_of, _with_indices_at_zero
 from ein.symbols import Index, Symbol, Variable
 from ein.type_system import Type, scalar
@@ -59,6 +58,12 @@ _WithIndex: TypeAlias = Callable[[Idx, T], T]
 
 def identity(x: T) -> T:
     return x
+
+
+def _layout_struct_to_expr(layout: Layout, struct) -> Expr:
+    return fold_layout(
+        layout, struct, lambda a: Array(a).expr, lambda a, b: calculus.Cons(a, b)
+    )
 
 
 def _infer_sizes(body: Expr, symbols: tuple[Symbol, ...], sizes) -> dict[Symbol, Expr]:
@@ -135,9 +140,7 @@ def structs(
     wrapped_indices = [Idx(Array(calculus.at(index))) for index in indices]
     cons = constructor(*wrapped_indices)
     layout = build_layout(cons, lambda a: Array(a).layout)
-    body: Expr = fold_layout(
-        layout, cons, lambda a: Array(a).expr, lambda a, b: calculus.Cons(a, b)
-    )
+    body: Expr = _layout_struct_to_expr(layout, cons)
     size_of = _infer_sizes(body, tuple(indices), size)
     for index in reversed(indices):
         body = calculus.Vec(index, size_of[index], body)
@@ -170,40 +173,30 @@ def fold(
     ...
 
 
+@overload
+def fold(init: StructLike, step: _WithIndex[Any], count: Size | None = None) -> Any:
+    ...
+
+
 def fold(init, step, count=None):
-    if not isinstance(init, tuple):
-        init = (init,)
-    k = len(init)
-    init_expr = functools.reduce(calculus.Cons, [Array(a).expr for a in init])
-
-    def take(e: calculus.Expr, i: int, n: int) -> calculus.Expr:
-        assert 0 <= i < n
-        if n <= 1:
-            return e
-        elif i + 1 == n:
-            return calculus.Second(e)
-        return take(calculus.First(e), i, n - 1)
-
-    def untuple(e: calculus.Expr, n: int) -> Array | tuple[Array, ...]:
-        if n == 1:
-            return Array(e)
-        return tuple(Array(take(e, i, n)) for i in range(n))
-
+    layout = build_layout(init, lambda a: Array(a).layout)
+    init_expr: Expr = _layout_struct_to_expr(layout, init)
     counter = calculus.variable(Variable(), scalar(int))
-    acc = calculus.variable(Variable(), init_expr.type)
     arg_index = Array(counter)
-    arg_acc = untuple(acc, k)
+    acc_var = Variable()
+    arg_acc = Array._maybe_tag(calculus.variable(acc_var, init_expr.type), layout)
     body = step(arg_index, arg_acc)
-    if not isinstance(body, tuple):
-        body = (body,)
-    if len(init) != len(body):
-        raise TypeError(
-            f"Mismatched number of tuple elements in "
-            f"accumulator initialiser and fold body: {len(init)} != {len(body)}"
+    layout_ = build_layout(init, lambda a: Array(a).layout)
+    if layout != layout_:
+        raise ValueError(
+            f"Expected initialiser and fold body layouts to match, "
+            f"got {layout} and {layout_}"
         )
-    body_expr = functools.reduce(calculus.Cons, [Array(a).expr for a in body])
+    body_expr: Expr = _layout_struct_to_expr(layout, body)
     size_of = _infer_sizes(
         body_expr, (counter.var,), (count,) if count is not None else None
     )
-    e = calculus.Fold(counter.var, size_of[counter.var], acc.var, init_expr, body_expr)
-    return untuple(e, k)
+    body = calculus.Fold(
+        counter.var, size_of[counter.var], acc_var, init_expr, body_expr
+    )
+    return Array._maybe_tag(body, layout)
