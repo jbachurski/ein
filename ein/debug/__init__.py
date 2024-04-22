54bd6f90b58b8a4773f08ae99fcb8dde889dbff1
jbachurski <kbachurski@gmail.com>
2024-04-23 01:32:09 +0200

    Export pretty_yarr_of_phi

--- ein/debug/__init__.py
+++ ein/debug/__init__.py
@@ -1,5 +1,5 @@
 from .graph import graph, plot_array_graph, plot_phi_graph, save_graph
-from .pprint import pretty_phi, pretty_yarr
+from .pprint import pretty_phi, pretty_yarr, pretty_yarr_of_phi
 
 __all__ = [
     "plot_phi_graph",
@@ -8,4 +8,5 @@ __all__ = [
     "save_graph",
     "pretty_phi",
     "pretty_yarr",
+    "pretty_yarr_of_phi",
 ]
